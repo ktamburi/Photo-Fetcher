a9ffb50d2f2fd7be9ad100fd51244dd23d48f107
Klaudia <ktamburi22@epoka.edu.al>
2025-12-03 11:52:52 +0100

    Update Header title in Layout component

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -3,7 +3,7 @@ import Header from '../Components/Header.jsx';
 function Layout({ children }) {
     return (
         <>
-            <Header children='Klaudias Photo Fetcher' />
+            <Header children='Photo Fetcher' />
             {children}
         </>
     );
